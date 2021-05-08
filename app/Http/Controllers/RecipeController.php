c3e6c96b5406d1df0fb0e9f109f37315df0b08d7
Hayden Bech <haydenbech@gmail.com>
2021-05-08 12:47:46 +0800

    Remove unused import

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RecipeRequest;
 use App\Models\Recipe;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
